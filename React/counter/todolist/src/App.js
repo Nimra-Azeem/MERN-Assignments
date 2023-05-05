a228694dc8af69c1888e79305a27bc91f6108e70
NimraAzeem <nimra.azeem@oomi.co.uk>
2023-05-05 01:55:13 +0100

    To do List

---
+++ React/counter/todolist/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ItemsList from './listitems.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        To Do List
+      </h1>
+      <ItemsList/>
+    </div>
+  );
+}
+
+export default App;
