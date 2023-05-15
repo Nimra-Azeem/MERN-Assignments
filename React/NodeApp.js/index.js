f0bad7c37a0f8b993cdb3ad65f88a66f099ef5aa
NimraAzeem <nimra.azeem@oomi.co.uk>
2023-05-15 08:11:20 +0100

    Stringify function

--- React/NodeApp.js/index.js
+++ React/NodeApp.js/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
-
 const app = express();
 const PORT = 4000;
 app.use(bodyParser.json());
--- React/NodeApp.js/routes/users.js
+++ React/NodeApp.js/routes/users.js
@@ -1,28 +1,41 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import fs from 'fs';
 
 const router = express.Router();
 const users = []
 router.get('/', (req, res) => {
     console.log(users);
-    res.send(users);
+    let data1 = [...users];
+    const data = JSON.stringify(data1);
+    let newdata;
+    let writedata;
+    try {
+        newdata = fs.writeFileSync('./routes/users.txt', data);
+        writedata = fs.readFileSync('./routes/users.txt', 'utf8');
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+    res.send(writedata);
 })
 
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
     const user = req.body;
-    users.push({...user, id:uuidv4()});
+    users.push({ ...user, id: uuidv4() });
     res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
-router.get('/:id', (req,res)=>{
-    const {id} = req.params;
-    const founduser = users.find((user) =>user.id==id);
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const founduser = users.find((user) => user.id == id);
     res.send(founduser);
 })
 
-router.delete('/:id', (req,res)=>{
-    const {id} = req.params;
-    users = users.filter((user) =>user.id!=id);
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id != id);
     res.send(`User with id ${id} deleted from the database.`);
 });
 
