490b0b6d3ae87cecacf8e5bd3af19ead2aa3c6d3
NimraAzeem <nimra.azeem@oomi.co.uk>
2023-05-15 06:41:22 +0100

    Apis changes

--- React/NodeApp.js/index.js
+++ React/NodeApp.js/index.js
@@ -5,6 +5,7 @@ import usersRoutes from './routes/users.js';
 const app = express();
 const PORT = 4000;
 app.use(bodyParser.json());
-app.use('/users', usersRoutes)
+app.use('/users', usersRoutes);
+
 app.listen(PORT, ()=>console.log(`Server Running on port: http://localhost ${PORT}`));
 app.get('/',(req,res)=>res.send('Hello from Homepage.'));
--- React/NodeApp.js/routes/users.js
+++ React/NodeApp.js/routes/users.js
@@ -1,23 +1,23 @@
 import express from 'express';
-import {v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
 const users = []
-
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     console.log(users);
     res.send(users);
 })
 
-router.post('/', (req, es)=>{
+router.post('/', (req, res)=>{
     const user = req.body;
     users.push({...user, id:uuidv4()});
-    res.send(`User with the name ${user.firstname} added to the database!`);
+    res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
 router.get('/:id', (req,res)=>{
     const {id} = req.params;
     const founduser = users.find((user) =>user.id==id);
-    res.send(foundUser);
+    res.send(founduser);
 })
 
 router.delete('/:id', (req,res)=>{
